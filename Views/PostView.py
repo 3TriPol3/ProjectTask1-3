b32ccc65e3c3064399ba873feb4e1ee30a005258
SystemAdminTkinter <gancster007@gmail.com>
2026-01-31 08:33:23 +0500

    ...

--- Views/PostView.py
+++ Views/PostView.py
@@ -17,28 +17,21 @@ class PostView(Tk):
         self.table_content = 0
 
         # Фрейм Добавить пост
-        self.add_frame = ttk.Frame(self,
-                                   borderwidth=1,
-                                   relief=SOLID,
-                                   padding=[18], # внутренние отступы фрейма
+        self.add_frame = ttk.Frame(self, borderwidth=1, relief=SOLID, padding=[18], # внутренние отступы фрейма
         )
         self.add_frame.pack(anchor=CENTER, fill=X, padx=10, pady=10)
+
         #Фрейм, в котором расположен текст Добавить Пост (Находится внутри фрейма add_frame)
         self.add_title_frame = ttk.Frame(self.add_frame, relief=SOLID, borderwidth=1, padding=[8, 10])
         self.add_title_frame.pack(anchor=CENTER, fill=X, padx=10, pady=10,)
 
         self.add_title = ttk.Label(self.add_title_frame, text="Добавить пост")
         self.add_title.pack()
+
         # Фрейм в котором расположены окна ввода данных о Посте (Находится внутри фрейма add_frame)
-        self.add_input_frame = ttk.Frame(self.add_frame,
-                                         relief=SOLID,
-                                         borderwidth=1,
-                                         padding=[8, 10]
-                                         )
-        self.add_input_frame.pack(fill=X,
-                                  padx=10,
-                                  pady=10,
-                                  )
+        self.add_input_frame = ttk.Frame(self.add_frame, relief=SOLID, borderwidth=1, padding=[8, 10])
+        self.add_input_frame.pack(fill=X, padx=10, pady=10)
+
         # Окна ввода данных Поста для добавления в таблицу БД
         self.add_title_title = ttk.Label(self.add_input_frame, text="Название")
         self.add_title_title.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
@@ -155,7 +148,6 @@ class PostView(Tk):
             self.author,
             self.created_date,
             self.views
-
         )
         # Обновить данные таблицы Treeview
         self.table()
